864c38b71391fade166994e67ac79034c60c6e37
Dhruv <dhruvrohilla9350@gmail.com>
2024-12-27 21:47:57 +0530

    littale change

--- day1/basic.java
+++ day1/basic.java
@@ -1,8 +1,8 @@
+import java.util.Scanner;
 public class basic {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
-        System.out.println("Hello World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next());
+        sc.close();
     }
-    
 }
